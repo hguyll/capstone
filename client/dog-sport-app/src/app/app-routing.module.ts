f03cc8bc063a66ceff8d937a3c566be41723286e
Heather Guyll <heather.guyll@jbhunt.com>
2021-09-15 11:58:48 -0500

    Code cleanup

--- client/dog-sport-app/src/app/app-routing.module.ts
+++ client/dog-sport-app/src/app/app-routing.module.ts
@@ -1,11 +1,6 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 
-import { ScentWorkComponent } from './sports/scent-work/scent-work.component';
-import { ConformationComponent } from './sports/conformation/conformation.component';
-import { DockDivingComponent } from './sports/dock-diving/dock-diving.component';
-import { ObedienceComponent } from './sports/obedience/obedience.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 
@@ -13,22 +8,6 @@ const routes: Routes = [
     {
         path: 'home',
         component: HomeComponent
-    },
-    {
-        path: 'sport/Scent Work',
-        component: ScentWorkComponent
-    },
-    {
-        path: 'sport/Conformation',
-        component: ConformationComponent
-    },
-    {
-        path: 'sport/Dock Diving',
-        component: DockDivingComponent
-    },
-    {
-        path: 'sport/Obedience',
-        component: ObedienceComponent
     },
     {
         path: 'login',
--- client/dog-sport-app/src/app/app.module.ts
+++ client/dog-sport-app/src/app/app.module.ts
@@ -4,10 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
 import { AppRoutingModule } from './app-routing.module';
-import { ConformationComponent } from './sports/conformation/conformation.component';
 import { DockDivingComponent } from './sports/dock-diving/dock-diving.component';
-import { ObedienceComponent } from './sports/obedience/obedience.component';
-import { ScentWorkComponent } from './sports/scent-work/scent-work.component';
 import { HomeComponent } from './home/home.component';
 import { HttpClientModule } from '@angular/common/http';
 import { TrialDetailComponent } from './sports/trial-detail/trial-detail.component';
@@ -22,10 +19,7 @@ import { SportComponent } from './sports/sport/sport.component';
   declarations: [
     AppComponent,
     NavComponent,
-    ConformationComponent,
     DockDivingComponent,
-    ObedienceComponent,
-    ScentWorkComponent,
     HomeComponent,
     TrialDetailComponent,
     CreateTrialComponent,
